eb5c81c5a402b124eceb8b8d53ba556b2015cbae
najma099 <najmakhatun0999@gmail.com>
2025-07-28 09:33:11 +0530

    all reusable components for login n register

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,13 @@
+import {CardWrapper} from '@/components/auth/card-wrapper'
+export const LoginForm = () => {
+    return(
+        <CardWrapper
+            headerLabel="Welcome back"
+            backButtonLabel="Don't have an Account?"
+            backButtonHref="/auth/register"
+            showSocial
+        >
+            Login Form!
+        </CardWrapper>
+    )
+}
