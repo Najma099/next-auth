bd35cc56a2702cb090bc1365a5347701712e6c9a
najma099 <najmakhatun0999@gmail.com>
2025-07-29 13:17:08 +0530

    Added SignOut

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,9 +1,20 @@
-import { auth} from '@/auth'
+import { auth } from '@/auth'
+import { Button } from '@/components/ui/button'
+import { signOutAction } from '@/actions/signout'
+
 const SettingPage = async () => {
-    const session = await auth();
-    return(
-        <div className='text-amber-100'>{JSON.stringify(session)}</div>
-    )
+  const session = await auth();
+
+  return (
+    <div className='text-amber-100'>
+      {JSON.stringify(session)}
+      <form action={signOutAction}>
+        <Button type='submit'>
+          Sign Out
+        </Button>
+      </form>
+    </div>
+  )
 }
 
 export default SettingPage;
